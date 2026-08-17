66bfc8b870eaaf4632ecf8f677fb713bad944211
megapixelmx <megapixelmx@gmail.com>
2026-08-17 01:46:04 -0600

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const SERVERCOLAB_CONFIG = {
+  GOOGLE_CLIENT_ID: "[AQUI_VA_EL_TOKEN]",
+  GOOGLE_API_KEY: "[AQUI_VA_EL_TOKEN]",
+  GOOGLE_DRIVE_SCOPES: "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive",
+  COLAB_NOTEBOOK_DRIVE_PATH: "server/create/0_3_5_MineColab_improved.ipynb",
+  COLAB_EXEC_ENDPOINT: "[AQUI_VA_TU_ENDPOINT_DE_BACKEND]",
+  PLAYIT_DEFAULT_TUNNEL: "playit",
+  MAX_RAM_MB_DEFAULT: 4096,
+  MAX_RAM_MB_LIMIT: 12000,
+  DRIVE_STORAGE_LIMIT_GB: 15
+};
